82f9651a0cd1b927cce0b321692420589bfcaa2c
panda <nigazz58@gmail.com>
2020-05-06 22:28:34 +0900

    Updates

--- precache-manifest.3701a803765a5a3c8b4ff6ee5edd0d03.js
+++ precache-manifest.3701a803765a5a3c8b4ff6ee5edd0d03.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "907a6b69fbcef00dce2af2deda02aa22",
+    "revision": "806ee316adfb9abdc4d6060cfa101c04",
     "url": "/movie/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie/static/js/2.09eadc7c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "785b14718567487bb2ce",
-    "url": "/movie/static/js/main.bb3530bd.chunk.js"
+    "revision": "15150a50bfbac47bc972",
+    "url": "/movie/static/js/main.a52ced45.chunk.js"
   },
   {
     "revision": "33c8d4e3d19328e5d148",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie/precache-manifest.5d471ef2fc610515f1be7bb5fd5e8542.js"
+  "/movie/precache-manifest.3701a803765a5a3c8b4ff6ee5edd0d03.js"
 );
 
 self.addEventListener('message', (event) => {
